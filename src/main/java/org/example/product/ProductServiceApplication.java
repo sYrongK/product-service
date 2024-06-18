120e0b4d71cc1f2fb88c6e1bdf7fa88b1ad67cd0
임소연 <syrongk10@10x10.co.kr>
2024-06-18 16:23:13 +0900

    build: config-server 연결 추가, spring.application.name 설정

--- src/main/java/org/example/product/ProductServiceApplication.java
+++ src/main/java/org/example/product/ProductServiceApplication.java
@@ -2,8 +2,9 @@ package org.example.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/product/controller/ProductController.java
@@ -0,0 +1,16 @@
+package org.example.product.controller;
+
+import org.example.core.wrapper.ResultResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/apis/product")
+public class ProductController {
+
+    @GetMapping("/home")
+    public ResultResponse<Void> home() {
+        return new ResultResponse<>();
+    }
+}
