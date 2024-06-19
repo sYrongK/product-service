04d1cc7d38fc013c7384600b7bbf47acf870e469
임소연 <syrongk10@10x10.co.kr>
2024-06-19 13:47:50 +0900

    api 변경

--- src/main/java/org/example/product/controller/ProductController.java
+++ src/main/java/org/example/product/controller/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/apis/product")
+@RequestMapping(value = "/apis")
 public class ProductController {
 
     @GetMapping("/home")
